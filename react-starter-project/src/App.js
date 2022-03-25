e885f88cc3f0df86ded5d6ca0782a512907a08d6
M. Tirado <mtirado1999@hotmail.com>
2022-03-24 17:50:19 -0700

    Implement featured post in React

--- react-starter-project/src/App.js
+++ react-starter-project/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 const blogTitle = "A Blog";
@@ -17,10 +16,31 @@ function Header(title) {
 	);
 }
 
+function samplePost() {
+	return {
+		title: "Sample post",
+		img: "poppy.jpg",
+		date: "Mar 24, 2022",
+		content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus mauris vitae ultricies leo integer malesuada nunc. Amet nisl purus in mollis nunc sed id semper risus. Eget sit amet tellus cras adipiscing enim eu turpis."
+	};
+}
+
+function FeaturedPost(post) {
+	return (
+		<div id="featured">
+			<img src={post.img}></img>
+			<h2>{post.title}</h2>
+			<p className="date">{post.date}</p>
+			<p>{post.content}</p>
+		</div>
+	);
+}
+
 function App() {
   return (
     <div className="App">
 		{Header(blogTitle)}
+		{FeaturedPost(samplePost())}
     </div>
   );
 }
