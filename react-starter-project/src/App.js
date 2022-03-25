c10c2ce378d4cda1202fe6af748c678e95293b89
M. Tirado <mtirado1999@hotmail.com>
2022-03-25 13:05:29 -0700

    samplePost() now accepts a custom title

--- react-starter-project/src/App.js
+++ react-starter-project/src/App.js
@@ -2,9 +2,9 @@ import Header from './components/Header'
 import PostList from './components/PostList'
 import FeaturedPost from './components/FeaturedPost'
 
-function samplePost() {
+function samplePost(title = "Sample Post") {
 	return {
-		title: "Sample post",
+		title: title,
 		image: "/poppy.jpg",
 		date: "Mar 24, 2022",
 		content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus mauris vitae ultricies leo integer malesuada nunc. Amet nisl purus in mollis nunc sed id semper risus. Eget sit amet tellus cras adipiscing enim eu turpis."
@@ -14,7 +14,7 @@ function samplePost() {
 function makePosts(count) {
 	const posts = [];
 	for(var i = 0; i < count; i++) {
-		posts.push(samplePost());
+		posts.push(samplePost(`Sample post ${i + 1}`));
 	}
 	return posts;
 }
@@ -23,7 +23,7 @@ function App() {
   return (
     <div className="App">
 		<Header title="My Blog"/>
-		<FeaturedPost post={samplePost()}/>
+		<FeaturedPost post={samplePost("Featured Post")}/>
 		<PostList posts={makePosts(5)}/>
     </div>
   );
