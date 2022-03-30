dba2322107469bd7870636a68e434c1a30fbabb7
M. Tirado <mtirado1999@hotmail.com>
2022-03-29 19:20:27 -0700

    Add example of routing in Express

--- backend-intro/index.js
+++ backend-intro/index.js
@@ -7,6 +7,11 @@ app.get('/', (request, response) => {
 	response.json({title: "Hello!", content: "These are the contents"})
 });
 
+app.get('/post/:postId', (request, response) => {
+	const postId = request.params.postId;
+	response.json({title: "A post", id: postId});
+});
+
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}...`);
 });
