7bd0e4ccb3278f9dbbb4655cb345fa89d20809bc
M. Tirado <mtirado1999@hotmail.com>
2022-04-04 09:32:50 -0700

    Use express router

--- backend-intro/src/index.js
+++ backend-intro/src/index.js
@@ -6,10 +6,7 @@ require('dotenv').config();
 
 const postsService = require('./service/posts');
 const commentsService = require('./service/comments');
-
 const PORT = 2400;
-app.use(express.json());
-app.use(cors());
 
 function connectDatabase() {
 	try {
@@ -20,15 +17,26 @@ function connectDatabase() {
 	}
 }
 
-app.get('/posts', postsService.getPosts);
-app.get('/posts/:postId', postsService.getPostById);
-app.post('/posts', postsService.createPost);
-app.delete('/posts/:postId', postsService.deletePost);
-app.put('/posts/:postId', postsService.updatePost);
+const commentsRouter = express.Router();
+commentsRouter.route('/')
+	.get(commentsService.getComments)
+	.post(commentsService.createComment);
+commentsRouter.route('/:commentId')
+	.delete(commentsService.deleteComment);
 
-app.get('/posts/:postId/comments', commentsService.getComments);
-app.post('/posts/:postId/comments', commentsService.createComment);
-app.delete('/posts/:postId/comments/:commentId', commentsService.deleteComment);
+const postsRouter = express.Router();
+postsRouter.route('/')
+	.get(postsService.getPosts)
+	.post(postsService.createPost);
+postsRouter.route('/:postId')
+	.get(postsService.getPostById)
+	.delete(postsService.deletePost)
+	.put(postsService.updatePost);
+postsRouter.use('/:postId/comments', commentsRouter);
+
+app.use(express.json());
+app.use(cors());
+app.use('/posts', postsRouter);
 
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}...`);
